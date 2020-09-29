3b01002cc5df02ac0b8c299d680d3496dcc0a89e
Christian Fritz <christian.fritz@gmail.com>
2020-09-29 11:26:42 -0700

    tf.test: start and stop our own static_transform_publishers for testing

--- tf.test.js
+++ tf.test.js
@@ -1,22 +1,40 @@
 const rosnodejs = require('rosnodejs');
 const tf = require('./tf.js');
+const { exec, spawn } = require('child_process');
 
 const assert = require('assert');
 
 rosnodejs.initNode('/tfjs_test').then(async (rosNode) => {
   tf.init(rosNode);
   setTimeout(() => console.log(JSON.stringify(tf.getForest(), 2, 2)), 500);
-  setTimeout(() => console.log(JSON.stringify(tf.getForest(), 2, 2)), 1500);
 });
 
 /** this test requires there being a publisher of the map to odom transform */
 describe('test TF', function() {
+  const processes = [];
+
+  before(function() {
+    console.log('starting some static publishers');
+    processes.push(spawn('rosrun',
+      'tf2_ros static_transform_publisher 1 0 0  0 0 0 f1 f2'.split(' ')));
+    processes.push(spawn('rosrun',
+      'tf2_ros static_transform_publisher 2 0 0  0 0 0 f1 f4'.split(' ')));
+    processes.push(spawn('rosrun',
+      'tf2_ros static_transform_publisher 0 1 0  0 0 0 f2 f3'.split(' ')));
+  })
+
+  after(function() {
+    console.log('shutting down');
+    processes.forEach(p => p.kill('SIGINT'));
+    rosnodejs.shutdown();
+  })
+
   it('should receive TFs from static', function(done) {
-    this.timeout(5000);
+    this.timeout(3000);
     setTimeout(() => {
-        const transform = tf.getTF('map','odom');
+        const transform = tf.getTF('f1','f3');
         assert(transform, 'received static TF');
         done();
-      }, 4000);
+      }, 1000);
   });
 });
